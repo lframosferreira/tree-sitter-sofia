ffa5f56b336962fde82540dd2d4e37a4e6d7d40f
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2025-02-14 18:41:04 -0300

    I have no idea why it is not working

--- grammar.js
+++ grammar.js
@@ -11,7 +11,35 @@ module.exports = grammar({
   name: "sofia",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._declaration),
+
+    _declaration: ($) => choice($.function_declaration, $.variable_declaration),
+
+    function_declaration: ($) =>
+      seq("func", $._type, $.identifier, $.parameter_list, $.block),
+
+    variable_declaration: ($) =>
+      seq("let", $._type, $.identifier, "=", $._expression, ";"),
+
+    parameter_list: ($) =>
+      seq(
+        "(",
+        // TODO: parameters
+        ")",
+      ),
+
+    _type: ($) => choice("bool", "int", "float", "string", "list"),
+
+    block: ($) => seq("{", repeat($._statement), "}"),
+
+    _statement: ($) => choice($.return_statement),
+
+    return_statement: ($) => seq("return", $._expression, ";"),
+
+    _expression: ($) => choice($.identifier, $.literal),
+
+    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+
+    literal: ($) => choice(/\d+/, /\d+\.\d+/, "True", "False"),
+  },
 });
